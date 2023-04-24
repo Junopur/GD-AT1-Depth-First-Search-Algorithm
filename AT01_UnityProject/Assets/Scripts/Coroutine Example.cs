895071a3f822d492db5d2d23b9a55b8b05c4966c
Mystirn <mystirns@gmail.com>
2023-04-24 10:20:00 +0800

    Added the damn project
    >:3

---
+++ AT01_UnityProject/Assets/Scripts/Coroutine Example.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private IEnumerator WaitAndPrint(float _withTime)
+   (
+    Debug.Log("First Message,so time has passed);
+    yield return new WaitForSeconds(_waitTime);
+    Debug.Log("Next Message, " + _waitTime + " seconds have passed");
+    yield return new waitForSeconds(_waitTime + " seconds have passed");
+    Debug.Log("Next Message, " + (_waitTime + if) + " seconds have passed")
+   )
+}
